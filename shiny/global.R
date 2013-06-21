e209acc504b9db5bc9ea62d733aa3329a3b6113f
Josh London <josh.london@noaa.gov>
2013-06-21 15:11:34 -0700

    ADD CHECK FOR REMOTE/LOCAL DATA PATH TO SHINY
    the local data path used for the shiny app is different on the shiny server than within the package. this checks and loads the proper path.

--- shiny/global.R
+++ shiny/global.R
@@ -15,7 +15,17 @@ PvGlmPredict<-function(glm_obj){
     return(pred)
 }
 
-iliamna_counts <- read.csv('data-open/Iliamna_HS_effort_count_weather.csv')
+# path to data file when on remote server
+data_file <- 'data-open/Iliamna_HS_effort_count_weather.csv'
+
+#check for data file in remote server path first. if not there, load local path
+if (file.exists(data_file)){
+    iliamna_counts <- read.csv(data_file)
+} else {
+    iliamna_counts <- read.csv('../data-open/Iliamna_HS_effort_count_weather.csv')
+}
+
+
 iliamna_counts$datetime <- as.POSIXct(iliamna_counts$datetime,tz="US/Alaska")
 
 iliamna_counts$year <- as.numeric(format(iliamna_counts$datetime,"%Y"))
@@ -23,3 +33,6 @@ iliamna_counts$hod <- as.numeric(format(iliamna_counts$datetime,"%H"))
 iliamna_counts$doy<-lubridate::yday(iliamna_counts$datetime)
 
 iliamna_grouped <- ddply(iliamna_counts,.(year,doy),summarize,totalcount = sum(nonpup_count,na.rm=TRUE),hod = mean(ifelse(hod==0,NA,hod)))
+iliamna_grouped$year <- as.integer(iliamna_grouped$year)
+iliamna_grouped$hod <- as.integer(iliamna_grouped$hod)
+iliamna_grouped$doy <- as.integer(iliamna_grouped$doy)
