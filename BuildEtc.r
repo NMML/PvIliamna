5a7ff5c5b23d71a1821f91a9f8cb7fcf8f337d35
Jay Ver Hoef <jay.verhoef@noaa.gov>
2013-07-24 11:02:39 -0800

    add /inst/rawdata folder to access rawdata from package

---
+++ BuildEtc.r
@@ -0,0 +1,65 @@
+# Roxygen to create help files
+cd "/media/Hitachi2GB/00NMML/RPackages/spPlotSampCourse_package"
+R
+library(roxygen2)
+roxygenize("spPlotSampCourse", roclets = c("namespace","rd"))
+
+# build R package in Linux
+cd "/media/Hitachi2GB/00NMML/ActiveRPack/PvIliamna_package/remote/pkg-source"
+R CMD build PvIliamna
+R CMD INSTALL PvIliamna_1.0.tar.gz
+
+# knitr
+cd "/media/Hitachi2GB/00NMML/RPackages/spPlotSampCourse_package/spPlotSampCourse/inst/doc/01Introduction"
+R
+library(knitr)
+knit("Introduction.Rnw")
+cd "/media/Hitachi2GB/00NMML/RPackages/spPlotSampCourse_package/spPlotSampCourse/inst/doc/02IntroToSpatialStat"
+R
+library(knitr)
+knit("IntroToSpatialStat.Rnw")
+cd "/media/Hitachi2GB/00NMML/RPackages/spPlotSampCourse_package/spPlotSampCourse/inst/doc/03BlockPred"
+R
+library(knitr)
+knit("BlockPred.Rnw")
+cd "/media/Hitachi2GB/00NMML/RPackages/spPlotSampCourse_package/spPlotSampCourse/inst/doc/04BlockPredFinGrid"
+R
+library(knitr)
+knit("BlockPredFinGrid.Rnw")
+cd "/media/Hitachi2GB/00NMML/RPackages/spPlotSampCourse_package/spPlotSampCourse/inst/doc/05CountSamp"
+R
+library(knitr)
+knit("CountSamp.Rnw")
+
+# windows 32-bit using VirtualBox
+cd E:\00NMML\RPackages\spPlotSampCourse_package
+Rcmd INSTALL --build spPlotSampCourse
+
+#-------------------------------------------------------------------------------
+#                    GITHUB
+#-------------------------------------------------------------------------------
+
+#github cloud commits
+cd "/media/Hitachi2GB/00NMML/ActiveRPack/PvIliamna_package"
+git add .
+git commit -a -m 'final push for course'
+git push github.PvIliamna master
+git pull github.PvIliamna master
+
+#to see an earlier version
+cd "/media/Hitachi2GB/00NMML/ActiveRPack/PvIliamna_package"
+git log splmm.R
+git show c742d1026aa031baf9409c0262b5bc944bb736d1:./covParmIni.R
+
+#initializing stuff
+cd "/media/Hitachi2GB/00NMML/ActiveRPack/PvIliamna_package"
+git init
+git config --global jay.verhoef 'Jay Ver Hoef'
+git config --global user.email jay.verhoef@noaa.gov
+git remote add github.PvIliamna https://github.com/NMML/PvIliamna.git
+git clone https://github.com/NMML/PvIliamna.git remote -v
+git remote -v
+
+#clean up directory before committing it
+cd "/media/Hitachi2GB/00NMML/RPackages/spPlotSampCourse_package"
+find . -type f -iname \*~ -delete
